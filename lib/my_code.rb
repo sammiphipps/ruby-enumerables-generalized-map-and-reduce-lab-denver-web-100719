89f8ea03ad2c6da6a06c1f1eaca104c5fe145959
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-20 17:49:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,12 +10,14 @@ end
 
 def reduce(source_array, starting_point = nil)
   if starting_point
-    num1 = starting_point
+    sum = starting_point
     i = 0
   else 
-    num1 = source_array[0]
+    sum = source_array[0]
     i = 1
   end
   
   while i < source_array.length
+    yield(sum, source_array[i])
+  end 
 end 
