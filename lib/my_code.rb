64511ddda9bf54b8f325d50c5c12a035da2d2a89
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-20 17:48:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,7 +10,12 @@ end
 
 def reduce(source_array, starting_point = nil)
   if starting_point
-    
+    num1 = starting_point
+    i = 0
   else 
+    num1 = source_array[0]
+    i = 1
   end
+  
+  while i < source_array.length
 end 
