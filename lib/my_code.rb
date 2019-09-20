d3b7b3863b836cbf197eeeaf295b5db9b10ac1ed
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-20 17:35:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ def reduce(source_array, starting_point = 0)
   i = 0
   while i < source_array.length do
     #yield(memo, n) needs to do memo + n, 
-    yield()
+    yield(if())
     i += 1
   end 
 end 
