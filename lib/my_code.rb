6d68719e641f867e1b6ecddcf846d287e5ce557d
Samantha Phipps <sammi.phipps@gmail.com>
2019-09-20 17:44:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,7 @@ def map(source_array)
   return new 
 end
 
-def reduce(source_array, starting_point = 0)
+def reduce(source_array, starting_point = nil)
   i = 0
   while i < source_array.length do
     yield()
